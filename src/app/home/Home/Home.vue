85d5f3b2a17a1b770ed3a996a435ce1ae58516d3
Dody <dodysenz@gmail.com>
2020-02-21 01:39:23 +0800

    Update Vision Section

--- src/app/home/Home/Home.vue
+++ src/app/home/Home/Home.vue
@@ -1,30 +1,34 @@
 <template>
   <div>
     <stage :disable-particles="disableParticles" />
-    <home-section
-      image="firmware.svg"
-      alternative
-    >
+    <home-section image="firmware.svg" alternative>
       <vue-headline level="2">About Us</vue-headline>
       <br />
       <vue-headline level="3"></vue-headline>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue sodales augue at consectetur. Etiam quis euismod lectus.</p>
-      <br />
-      <vue-headline level="3">Lorem Ipsum</vue-headline>
       <p>
-        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus a congue eros, ac condimentum est. Integer lectus eros, pellentesque ac ante eu, rhoncus vulputate nibh. Fusce sed condimentum felis.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue sodales augue at consectetur. Etiam quis
+        euismod lectus.
       </p>
       <br />
       <vue-headline level="3">Lorem Ipsum</vue-headline>
       <p>
-        Proin massa tellus, accumsan at facilisis et, iaculis vel ex. Nunc quis egestas turpis, non congue nisl. Donec eu mi faucibus, lobortis sem eu, placerat nisi.
+        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus a congue
+        eros, ac condimentum est. Integer lectus eros, pellentesque ac ante eu, rhoncus vulputate nibh. Fusce sed
+        condimentum felis.
+      </p>
+      <br />
+      <vue-headline level="3">Lorem Ipsum</vue-headline>
+      <p>
+        Proin massa tellus, accumsan at facilisis et, iaculis vel ex. Nunc quis egestas turpis, non congue nisl. Donec
+        eu mi faucibus, lobortis sem eu, placerat nisi.
       </p>
     </home-section>
-
-    <home-section
-      image="maker.svg"
-      flip
-    >
+    <div :class="$style.vision" ref="vision">
+      <span :class="$style.bigspan">VISION</span>
+      <span :class="$style.smallspan">vision</span>
+      <p>To Nurture A Committed Robotics Community</br>By Building Skills To Take On Industry Projects.</p>
+    </div>
+    <home-section image="maker.svg" flip>
       <vue-headline level="2">Vision</vue-headline>
       <br />
       <vue-headline level="3">Lorem Ipsum</vue-headline>
@@ -34,7 +38,8 @@
       <br />
       <vue-headline level="3">Lorem Ipsum</vue-headline>
       <p>
-        Nunc tincidunt consectetur neque, at posuere lectus volutpat quis. Fusce auctor ante ligula, at laoreet diam aliquet at. Donec varius nibh ac turpis hendrerit, id porta tellus fringilla.
+        Nunc tincidunt consectetur neque, at posuere lectus volutpat quis. Fusce auctor ante ligula, at laoreet diam
+        aliquet at. Donec varius nibh ac turpis hendrerit, id porta tellus fringilla.
       </p>
       <br />
       <vue-headline level="3">Lorem Ipsum</vue-headline>
@@ -43,42 +48,41 @@
       </p>
     </home-section>
 
-    <home-section
-      image="teamwork.svg"
-      alternative
-    >
+    <home-section image="teamwork.svg" alternative>
       <vue-headline level="2">Projects</vue-headline>
       <br />
       <vue-headline level="3">Lorem Ipsum</vue-headline>
       <p>
-        Etiam magna mauris, lobortis sed felis id, facilisis gravida augue. Donec blandit, purus eu gravida maximus, neque dui luctus libero, sit amet maximus nunc tellus vitae lorem.
+        Etiam magna mauris, lobortis sed felis id, facilisis gravida augue. Donec blandit, purus eu gravida maximus,
+        neque dui luctus libero, sit amet maximus nunc tellus vitae lorem.
       </p>
       <br />
       <vue-headline level="3">Lorem Ipsum</vue-headline>
       <p>
-        Morbi laoreet porttitor dui, a lacinia mauris commodo ut. Phasellus feugiat, metus nec fermentum faucibus, nisi eros sodales nunc, cursus ultricies ipsum ex ut mi.
+        Morbi laoreet porttitor dui, a lacinia mauris commodo ut. Phasellus feugiat, metus nec fermentum faucibus, nisi
+        eros sodales nunc, cursus ultricies ipsum ex ut mi.
       </p>
       <br />
       <vue-headline level="3">Lorem Ipsum</vue-headline>
       <p>
-        Donec sit amet urna ut metus laoreet consectetur at quis mauris. Proin mollis ante non augue efficitur, quis sagittis ante porta.
+        Donec sit amet urna ut metus laoreet consectetur at quis mauris. Proin mollis ante non augue efficitur, quis
+        sagittis ante porta.
       </p>
     </home-section>
 
-    <home-section
-      image="hangout.svg"
-      flip
-    >
+    <home-section image="hangout.svg" flip>
       <vue-headline level="2">The Team</vue-headline>
       <br />
       <vue-headline level="3">Lorem Ipsum</vue-headline>
       <p>
-        Nam ultrices posuere leo, id tincidunt augue vulputate quis. Mauris rutrum eget odio nec interdum. Nulla eget arcu at mauris hendrerit commodo.
+        Nam ultrices posuere leo, id tincidunt augue vulputate quis. Mauris rutrum eget odio nec interdum. Nulla eget
+        arcu at mauris hendrerit commodo.
       </p>
       <br />
       <vue-headline level="3">Lorem Ipsum</vue-headline>
       <p>
-        Sed eu nunc tristique, tempus justo ut, placerat nisi. Ut tempus a diam eget porttitor. Curabitur aliquet pretium mi in sagittis. Donec ac felis turpis. 
+        Sed eu nunc tristique, tempus justo ut, placerat nisi. Ut tempus a diam eget porttitor. Curabitur aliquet
+        pretium mi in sagittis. Donec ac felis turpis.
       </p>
     </home-section>
   </div>
@@ -97,8 +101,7 @@ export default {
     meta: [
       {
         name: 'description',
-        content:
-          'sutdsoar - SUTD Organisation of Autonomous Robotics Website',
+        content: 'sutdsoar - SUTD Organisation of Autonomous Robotics Website',
       },
       {
         name: 'robots',
@@ -115,13 +118,11 @@ export default {
       { name: 'og:locale', content: 'en' },
       {
         name: 'og:title',
-        content:
-          'sutdsoar - SUTD Organisation of Autonomous Robotics Website',
+        content: 'sutdsoar - SUTD Organisation of Autonomous Robotics Website',
       },
       {
         name: 'og:description',
-        content:
-          'sutdsoar - SUTD Organisation of Autonomous Robotics Website',
+        content: 'sutdsoar - SUTD Organisation of Autonomous Robotics Website',
       },
       { name: 'og:image:url', content: 'https://sutdsoar.herokuapp.com/logo.svg' },
 
@@ -129,15 +130,13 @@ export default {
       { name: 'twitter:creator', content: '@jamestiotio' },
       {
         name: 'twitter:title',
-        content:
-          'sutdsoar - SUTD Organisation of Autonomous Robotics Website',
+        content: 'sutdsoar - SUTD Organisation of Autonomous Robotics Website',
       },
       { name: 'twitter:url', content: 'https://sutdsoar.herokuapp.com/' },
       { name: 'twitter:image', content: 'https://sutdsoar.herokuapp.com/logo.svg' },
       {
         name: 'twitter:description',
-        content:
-          'sutdsoar - SUTD Organisation of Autonomous Robotics Website',
+        content: 'sutdsoar - SUTD Organisation of Autonomous Robotics Website',
       },
     ],
   },
@@ -154,3 +153,37 @@ export default {
   },
 };
 </script>
+<style lang="scss" module>
+@import '~@/app/shared/design-system';
+.vision {
+  width: 100%;
+  height: 45vh;
+  background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="1923.617" height="584" viewBox="0 0 1923.617 584"><path id="Path_1160" data-name="Path 1160" d="M0,0S-70.5,3.529,411.016,64.927c458.3,59.788,645.477,12.188,901.414,0s-97.969,101.263,67.376,87.382S1920,0,1920,0V584H0Z" transform="translate(3.617)" fill="%23980031"/></svg>');
+  background-size: cover;
+
+  text-align: center;
+
+  padding-top: 14rem;
+
+  display: flex;
+  flex-flow: column;
+  .bigspan {
+    font-size: 6rem;
+    font-weight: 900;
+    letter-spacing: $header-background-letter-spacing;
+    opacity: $header-background-ghost-opacity;
+  }
+  .smallspan {
+    margin-top: -1.5rem;
+    font-size: 3rem;
+    font-weight: 900;
+    letter-spacing: $header-background-letter-spacing;
+    z-index: 2;
+  }
+  p {
+    font-size: 3rem;
+    margin-top: 3rem;
+    line-height: 6rem;
+  }
+}
+</style>
