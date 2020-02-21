71666ba1a15b550246ee96cd3b1c24ce560b02c2
Dody <dodysenz@gmail.com>
2020-02-21 11:01:32 +0800

    Update README

--- src/app/home/Home/Home.spec.ts
+++ src/app/home/Home/Home.spec.ts
@@ -1,9 +1,10 @@
+import { i18n } from '@shared/plugins/i18n/i18n';
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import Vuex from 'vuex';
-import { i18n } from '@shared/plugins/i18n/i18n';
-import Home from './Home.vue';
-import Stage from '../components/Stage/Stage.vue';
+
 import HomeSection from '../components/HomeSection/HomeSection.vue';
+import Stage from '../components/Stage/Stage.vue';
+import Home from './Home.vue';
 
 const localVue = createLocalVue();
 
@@ -29,6 +30,6 @@ describe('Home.vue', () => {
     });
 
     expect(wrapper.findAll(Stage)).toHaveLength(1);
-    expect(wrapper.findAll(HomeSection)).toHaveLength(4);
+    expect(wrapper.findAll(HomeSection)).toHaveLength(3);
   });
 });
